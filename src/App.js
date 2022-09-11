e3d575ee5151d79f97ae590866e6a15341b45115
sabinabale <104379027+sabinabale@users.noreply.github.com>
2022-09-11 12:25:40 +0200

    github link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <p>
+        This project was coded by Sabina and is hosted on <a href="">GitHub</a>
+      </p>
     </div>
   );
 }
